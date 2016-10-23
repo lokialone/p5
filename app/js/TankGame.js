62601f6c8c77f8c4752666ec035b4fc9cd853296
lokialone <648875862@qq.com>
2016-10-23 22:35:53 +0800

    move tank

--- app/js/TankGame.js
+++ app/js/TankGame.js
@@ -6,8 +6,8 @@ var Bullet = function(x,y) {
   this.y = y;
   this.grap = 4;
 }
-Bullet.prototype.render = function(p) {
 
+Bullet.prototype.render = function(p) {
   p.rect(this.x - this.bulletSize / 2, this.y - this.bulletSize,this.bulletSize,this.bulletSize);
 }
 
@@ -16,31 +16,67 @@ Bullet.prototype.update = function(){
    if(this.y <= 0){
      this.y = this.orgin_y;
    }
-
 }
+
 var Tank = function(){
+
   this.tankWidth = 50;
   this.tankHeight = 40;
 
+  this.gunturretWidth = 30;
+  this.gunturretHeight = 20;
+
   this.cannonHeight = 30;
   this.cannonDiameter = 6;
-  this.difference = 10;
+
   // tank的圆角
   this.borderRadius = 5;
   this.slotBulletFlag = true;
+  // tank的重心坐标
+  this.center_x = 0;
+  this.center_y = 0;
+
+  this.step = 10;
 }
 
 Tank.prototype.switchSlotBulletFlag = function(status) {
   this.slotBulletFlag = status;
 }
 
+Tank.prototype.setCenter =  function(p){
+  this.center_x = p.width / 2;
+  this.center_y = p.height - this.tankHeight / 2;
+}
+
+Tank.prototype.updateCenter = function(x,y){
+  this.center_x = x;
+  this.center_y = y;
+}
+
+Tank.prototype.turnLeft = function(p){
+  console.log('lefe');
+  // p.translate(this.center_x, this.center_y);
+  p.rotate(p.PI/2);
+}
+
+Tank.prototype.goForward = function(){
+  this.center_y -= this.step;
+}
+
+Tank.prototype.goBack = function() {
+  this.center_y += this.step;
+}
 Tank.prototype.render = function(p) {
+
+  // 坦克的body
   p.fill(255, 204, 0);
-  p.rect(p.width/2 - this.tankWidth/2, p.height - this.tankHeight, this.tankWidth, this.tankHeight,this.borderRadius);
+  p.rect(this.center_x - this.tankWidth / 2, this.center_y - this.tankHeight / 2, this.tankWidth, this.tankHeight,this.borderRadius);
+  //坦克的炮台
   p.fill(153, 204, 0);
-  p.rect(p.width/2 - this.tankWidth/2 + this.difference , p.height - this.tankHeight + this.difference, this.tankWidth - 2 * this.difference, this.tankHeight - 2 * this.difference,this.borderRadius);
+  p.rect(this.center_x - this.gunturretWidth / 2 , this.center_y - this.gunturretHeight / 2, this.gunturretWidth, this.gunturretHeight,this.borderRadius);
+  // 坦克的大炮
   p.fill(255,255,255);
-  p.rect(p.width/2 - this.cannonDiameter/2,p.height - this.tankHeight + this.difference - this.cannonHeight,this.cannonDiameter,this.cannonHeight);
+  p.rect(this.center_x - this.cannonDiameter / 2, this.center_y - this.gunturretHeight / 2 - this.cannonHeight,this.cannonDiameter,this.cannonHeight);
 
 }
 
@@ -59,22 +95,26 @@ var TankGame = function(p){
     p.createCanvas(800,640);
     p.background(224);
     p.fill(255,204,0);
+    tank.setCenter(p);
     bullet = new Bullet(400,580);
-    // for(var i = 0; i < bulletsCounts; i++){
-    //   bullets[i] = new Bullet(p.width,p.height - tank.tankHeight + tank.difference - tank.cannonHeight);
-    // }
+
   }
   p.draw = function() {
     p.background(224);
+    if (p.keyIsDown(p.LEFT_ARROW)) {
+
+    }
+    if (p.keyIsDown(p.UP_ARROW)) {
+      tank.goForward();
+    }
+    if (p.keyIsDown(p.DOWN_ARROW)) {
+        tank.goBack();
+    }
     tank.render(p);
     bullet.render(p);
     bullet.update();
-    // for(var i = 0; i < bulletsCounts; i++){
-    //   bullets[i].update();
-    //   bullets[i].render(p);
-    // }
-
   }
+
 }
 
 module.exports = TankGame;
